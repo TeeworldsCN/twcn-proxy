ed88adb9f02e2a1b0b9a077e2a4651ed82344208
Freddie Wang <tsfreddiewang@gmail.com>
2021-03-06 19:01:26 +0800

    player query

--- src/ddnet/players.ts
+++ src/ddnet/players.ts
@@ -1,9 +1,17 @@
 import { Route } from '../types';
+import { DateTime } from 'luxon';
 import cheerio from 'cheerio';
 
+export const toTimestamp = (ddnetTime: string) => {
+  const time = DateTime.fromISO(`${ddnetTime.slice(0, 10)}T${ddnetTime.slice(11, 16)}+0100`);
+  if (time.isValid) return time.toMillis();
+  return 0;
+};
+
 export const players: Route = (app, axios) => {
+  // Rank
   app.get('/ddnet/players', async (request, reply) => {
-    const response = await axios.get('https://ddnet.tw/ranks');
+    const response = await axios.get('/ranks');
     const $ = cheerio.load(response.data);
 
     const playerTable: [string, number][] = [
@@ -15,22 +23,108 @@ export const players: Route = (app, axios) => {
     ];
 
     const result: any = {};
+    const query = $(`#global .ladder .tight`);
     for (let [category, index] of playerTable) {
       result[category] = [];
-
-      $(`#global .ladder .tight`)
+      query
         .eq(index)
         .find('tr')
         .each((index, e) => {
           result[category].push({
-            position: index + 1,
+            rank: index + 1,
             name: $(`a`, e).text(),
             points: parseInt($(`.points`, e).text()),
-            country: $(`img`, e).attr('alt'),
+            server: $(`img`, e).attr('alt'),
           });
         });
     }
 
+    reply.send(result);
+  });
+
+  app.get('/ddnet/players/:player', async (request, reply) => {
+    const player: string = (request.params as any).player;
+    const playerQuery = encodeURIComponent(player.replace(/-/g, '-45-'));
+
+    const response = await axios.get(`/players/${playerQuery}`);
+
+    const $ = cheerio.load(response.data);
+
+    const result: any = {
+      name: player,
+    };
+
+    const playerTable: [string, number][] = [
+      ['points', 0],
+      ['teamRank', 1],
+      ['rank', 2],
+      ['monthlyPoints', 3],
+      ['weeklyPoints', 4],
+    ];
+
+    const queryBlock2 = $(`#global .block2.ladder .pers-result`);
+    for (let [category, index] of playerTable) {
+      const data = queryBlock2.eq(index).text().split(' ');
+
+      if (data.length == 4) {
+        result[category] = {
+          rank: parseInt(data[0]),
+          points: parseInt(data[2]),
+        };
+      }
+    }
+
+    result.server = queryBlock2.eq(5).find('img').attr('alt');
+
+    const queryFirstFinish = $(`#global .block2.ladder .personal-result`)
+      .text()
+      .match(/([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}): (.*) \((.*):(.*)\)/);
+
+    if (queryFirstFinish) {
+      result.firstFinish = {
+        timestamp: toTimestamp(queryFirstFinish[1]),
+        map: queryFirstFinish[2],
+        time: parseInt(queryFirstFinish[3]) * 60 + parseInt(queryFirstFinish[4]),
+      };
+    }
+
+    const queryBlock6 = $(`#global .block6.ladder .tight`);
+    const queryLastFinishes = queryBlock6.eq(0).find('td');
+
+    const lastFinishes: any = [];
+
+    queryLastFinishes.each((index, e) => {
+      const queryLine = $(e)
+        .text()
+        .match(/([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}):  ([^:]*): (.*) \((.*):(.*)\)/);
+
+      lastFinishes.push({
+        timestamp: toTimestamp(queryLine[1]),
+        type: queryLine[2],
+        map: queryLine[3],
+        time: parseInt(queryLine[4]) * 60 + parseInt(queryLine[5]),
+        server: $('img', e).attr('alt'),
+      });
+    });
+
+    result['lastFinishes'] = lastFinishes;
+
+    const queryFavoritePartners = queryBlock6.eq(1).find('td');
+
+    const favoritePartners: any = [];
+
+    queryFavoritePartners.each((index, e) => {
+      const queryLine = $(e)
+        .text()
+        .match(/[0-9]*. (.*): ([0-9]*)/);
+
+      favoritePartners.push({
+        name: queryLine[1],
+        ranks: parseInt(queryLine[2]),
+      });
+    });
+
+    result['favoritePartners'] = favoritePartners;
     reply.send(result);
   });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,58 @@
-import axios from 'axios';
+import { AxiosError } from 'axios';
 import fastify from 'fastify';
 import { ddnet } from './ddnet';
+import cheerio from 'cheerio';
+import { setup, RedisDefaultStore } from 'axios-cache-adapter';
+import redis from 'redis';
 
-const app = fastify({ logger: true });
-const axiosInstance = axios.create({
+require('dotenv').config();
+
+let store;
+if (process.env.REDIS_URL) {
+  const client = redis.createClient({
+    url: process.env.REDIS_URL,
+  });
+  store = new RedisDefaultStore(client, {
+    prefix: 'api',
+  });
+}
+
+const app = fastify({ logger: false });
+const ddnetAxios = setup({
+  baseURL: 'https://ddnet.tw',
   headers: {
     'Accept-Encoding': 'gzip, deflate',
   },
   decompress: true,
   timeout: 10000,
+  cache: {
+    maxAge: 10 * 60 * 1000,
+    store,
+  },
 });
 
-ddnet(app, axiosInstance);
+ddnet(app, ddnetAxios);
+
+app.setErrorHandler(function (error, request, reply) {
+  if ((error as any).isAxiosError) {
+    let e = error as AxiosError;
+
+    if (request.routerPath.startsWith('/ddnet')) {
+      reply.status(e.response.status).send({
+        statusCode: e.response.status,
+        error: cheerio.load(e.response.data)('title').text().replace(' - DDraceNetwork', ''),
+        message: e.message,
+      });
+      return;
+    }
+  }
+
+  reply.status(500).send({
+    statusCode: 500,
+    error: 'Internal Server Error',
+    message: error.message,
+  });
+});
 
 // Run the server!
 const start = async () => {
